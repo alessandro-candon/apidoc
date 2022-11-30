71d45cf69ea386d275e7604a6a6c14afddcc7ffd
Mauro <mauro.barzaghi@decathlon.com>
2022-11-30 18:21:09 +0100

    openapi option file updated with the jwt info

--- src/openapi.js
+++ src/openapi.js
@@ -3,20 +3,19 @@ const postmanToOpenApi = require("postman-to-openapi");
 const createOpenApiFile = (outPath, collection) => {
   const options = {
     auth: {
-      oAuth2: {
+      "jwt": {
         type: 'http',
         scheme: 'bearer',
-        bearerFormat: 'A resource owner JWT',
-        description: 'OAuth2 JWT token'
-      },
+        bearerFormat: 'JWT'
+        },
     },
     servers: [
       {
-        url: 'https://api.preprod.xxx.net',
+        url: 'https://api.preprod.decathlon.net',
         description: 'PreProduction environment server'
       },
       {
-        url: 'https://api.preprod.xxx.net',
+        url: 'https://api.preprod.decathlon.net',
         description: 'Production environment server'
       }
     ]
